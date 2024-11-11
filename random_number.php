6c879f856b061859386cce65ec5fb315008ea295
Andreyka9999 <andrey@poddubnyak.com>
2024-11-11 18:44:05 +0200

    KAN-24 completed

---
+++ random_number.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+
+    // The rand() function is used form generating random number, rand(min, max). The echo function displays generated number on webpage.
+    echo(rand(1,100));
+
+    ?>
+
+
+
+</body>
+</html>
